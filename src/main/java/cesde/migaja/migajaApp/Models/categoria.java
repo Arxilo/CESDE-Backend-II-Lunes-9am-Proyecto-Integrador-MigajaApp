2531c4d8e6f9f19826c1232baeb045e80bf4611f
Nicolas Alvarez <nicolas210825@gmail.com>
2026-03-02 10:41:43 -0500

    FEATURE/Nicolas-Alvarez: Subiendo rama de modelo de categoria

--- src/main/java/cesde/migaja/migajaApp/Models/categoria.java
+++ src/main/java/cesde/migaja/migajaApp/Models/categoria.java
@@ -1,5 +1,123 @@
 package cesde.migaja.migajaApp.Models;
 
+@Entity
+@Table(name = "categorias")
 public class categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String fechaCreacion;
+    private String responsable;
+    private String justificacion;
+    private String descripcion;
+    private Integer prioridad;
+    private String color;
+    private String icono;
+    private Boolean estado;
+
+    // Constructor vacío
+    public Categoria() {
+    }
+
+    // Constructor lleno
+    public Categoria(Integer id, String nombre, String fechaCreacion, String responsable, String justificacion, String descripcion, Integer prioridad, String color, String icono, Boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaCreacion = fechaCreacion;
+        this.responsable = responsable;
+        this.justificacion = justificacion;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.color = color;
+        this.icono = icono;
+        this.estado = estado;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public String getJustificacion() {
+        return justificacion;
+    }
+
+    public void setJustificacion(String justificacion) {
+        this.justificacion = justificacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
 }
+
+
+
+    
