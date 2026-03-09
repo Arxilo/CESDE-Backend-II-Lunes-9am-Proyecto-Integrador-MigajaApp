30d6f26febcf9c97201f96ff4e7c7839a6223af7
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-03-09 11:20:59 -0500

    Repaso clase

--- src/main/java/cesde/migaja/migajaApp/Models/categoria.java
+++ src/main/java/cesde/migaja/migajaApp/Models/categoria.java
@@ -1,5 +1,9 @@
 package cesde.migaja.migajaApp.Models;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 @Entity
 @Table(name = "categorias")
 public class categoria {
---
+++ src/main/java/cesde/migaja/migajaApp/Models/utils/TipoDocumento.java
@@ -0,0 +1,10 @@
+package cesde.migaja.migajaApp.Models.utils;
+
+public enum TipoDocumento {
+
+    Cedula,
+    Pasaporte,
+    Tarjeta_Identidad,
+    Registro_civil
+
+}
