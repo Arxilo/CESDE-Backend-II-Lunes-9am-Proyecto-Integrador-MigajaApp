014b7ba1564c5f273b9a093927b185c038ca9be5
Migaja-App <aprendendoportuguesmateo@gmail.com>
2026-03-20 17:46:52 -0500

    relaciones tablas

--- src/main/java/cesde/migaja/migajaApp/Models/Gasto.java
+++ src/main/java/cesde/migaja/migajaApp/Models/Gasto.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name = "gastos")
