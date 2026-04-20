58b4d64c41903f7c73228bddecd38a60e1abdbd2
Arxilo <matearcila22@gmail.com>
2026-04-20 09:52:05 -0500

    Subiendo cambios recientes al proyecto

--- src/main/java/cesde/migaja/migajaApp/repositorios/IUsuarioRepositorio.java
+++ src/main/java/cesde/migaja/migajaApp/repositorios/IUsuarioRepositorio.java
@@ -1,15 +1,36 @@
 package cesde.migaja.migajaApp.repositorios;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import cesde.migaja.migajaApp.Models.Usuario;
+import cesde.migaja.migajaApp.Models.utils.TipoDocumento;
+
 
 @Repository
 public interface IUsuarioRepositorio extends JpaRepository<Usuario , Integer>{
 
 
-    
+    List<Usuario> findByNombre(String nombre);
+
+    List<Usuario> findByTipoDocumento(TipoDocumento tipoDocumento);
+
+    Optional<Usuario> findByDocumento(String documento);
+
+    List<Usuario> findByEdad(Integer edad);
+
+
+
+
+
+
+
+
+
+
     
 
 }
