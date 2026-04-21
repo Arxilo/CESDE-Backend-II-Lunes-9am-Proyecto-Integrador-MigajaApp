5a7f4095ae1b0e76be2c3f92d7d6a1c467bda100
Luz Marycelda Zabala <maryzabala52g@gmail.com>
2026-04-21 16:20:20 -0500

    Agrego validaciones de medios de pago

--- src/main/java/cesde/migaja/migajaApp/servicios/Serviciomediodepago.java
+++ src/main/java/cesde/migaja/migajaApp/servicios/Serviciomediodepago.java
@@ -1,8 +1,34 @@
 package cesde.migaja.migajaApp.servicios;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import cesde.migaja.migajaApp.Models.MedioPago;
+import cesde.migaja.migajaApp.repositorios.Imediodepagorepositorio;
 
 @Service
 public class Serviciomediodepago {
+    @Autowired 
+    private Imediodepagorepositorio mediodepagorepositorio;
 
+    public MedioPago guardar_medioPago(MedioPago datosmediopago){
+
+
+        if (datosmediopago.getNombre().isEmpty()||datosmediopago.getNombre().isBlank()||datosmediopago.getNombre()==null){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Dato requerido"
+            );
+        }
+        return mediodepagorepositorio.save(datosmediopago);
+    }
+
+    public List<MedioPago> listar_mediopago(){
+        return mediodepagorepositorio.findAll();
+    }
 }
