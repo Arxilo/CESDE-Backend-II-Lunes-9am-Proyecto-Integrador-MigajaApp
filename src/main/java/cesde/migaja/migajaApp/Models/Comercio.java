20197c9c57a7d2d062534e95e9b8cf278c4f35e9
Arxilo <matearcila22@gmail.com>
2026-05-06 17:42:07 -0500

    ok

--- src/main/java/cesde/migaja/migajaApp/Models/Comercio.java
+++ src/main/java/cesde/migaja/migajaApp/Models/Comercio.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 
@@ -39,11 +41,10 @@ public class Comercio {
     private String representanteLegal;
 
     //Relacion "un gasto, muchos comercios"
-    @ManyToOne(mappedBy = "comercio")
-    @joinColumn(name = "comercios", referencedColumnName = "id")
+    @ManyToOne
+    @JoinColumn(name = "comercios", referencedColumnName = "id")
     private Gasto gasto;
-    
-    // metodo comercio gasto
+
     public Comercio() {
     }
 
