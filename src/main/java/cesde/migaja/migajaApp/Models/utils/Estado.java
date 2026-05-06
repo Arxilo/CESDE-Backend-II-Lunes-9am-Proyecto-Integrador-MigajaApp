e16c625d7aaf5eacdbd291a8a7bc3cebc2159ad6
Arxilo <matearcila22@gmail.com>
2026-05-06 15:33:04 -0500

    ok

---
+++ src/main/java/cesde/migaja/migajaApp/Models/utils/Estado.java
@@ -0,0 +1,8 @@
+package cesde.migaja.migajaApp.Models.utils;
+
+public enum Estado {
+
+    ACTIVO,
+    INACTIVO
+
+}
---
+++ src/main/java/cesde/migaja/migajaApp/Models/utils/Prioridad.java
@@ -0,0 +1,9 @@
+package cesde.migaja.migajaApp.Models.utils;
+
+public enum Prioridad {
+
+    BAJA,
+    MEDIA,
+    ALTA
+
+}
