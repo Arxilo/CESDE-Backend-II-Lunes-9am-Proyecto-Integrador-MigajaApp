0b54426aaf5b7364053d66fe48c1cd04f6b0499a
Emma <zuluagamoraemmanuel@gmail.com>
2026-05-09 20:24:11 -0500

    Feature: crud terminado en el servicio e implementados en el controller

--- src/main/java/cesde/migaja/migajaApp/servicios/GastoServicio.java
+++ src/main/java/cesde/migaja/migajaApp/servicios/GastoServicio.java
@@ -1,6 +1,7 @@
 package cesde.migaja.migajaApp.servicios;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,5 +35,54 @@ public class GastoServicio {
 
     public List<Gasto> listar_gastos() {
         return repositorio.findAll();
+    }
+
+    public Gasto actualizarGasto(Long id, Gasto gasto) {
+        Optional<Gasto> gastoBusqueda = repositorio.findById(id);
+
+        if (gastoBusqueda.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Gasto no se encontro");
+        } else {
+            Gasto gastoEncontrado = gastoBusqueda.get();
+            gastoEncontrado.setDescripcion(gasto.getDescripcion());
+            gastoEncontrado.setFecha(gasto.getFecha());
+            gastoEncontrado.setMonto(gasto.getMonto());
+            gastoEncontrado.setImagen(gasto.getImagen());
+            gastoEncontrado.setMoneda(gasto.getMoneda());
+            gastoEncontrado.setMetodoPago(gasto.getMetodoPago());
+            gastoEncontrado.setLugar(gasto.getLugar());
+            gastoEncontrado.setEsRecurrente(gasto.getEsRecurrente());
+            gastoEncontrado.setTipoGasto(gasto.getTipoGasto());
+            gastoEncontrado.setImpactoFinanciero(gasto.getImpactoFinanciero());
+            gastoEncontrado.setActivo(gasto.getActivo());
+            gastoEncontrado.setObservaciones(gasto.getObservaciones());
+
+            return repositorio.save(gastoEncontrado);
+        }
+    }
+
+    public Boolean eliminarGasto(Long id) {
+        Optional<Gasto> gasto = repositorio.findById(id);
+        if (gasto.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Gasto no se encontro");
+        } else {
+            repositorio.deleteById(id);
+            return true;
+        }
+    }
+
+    public Gasto buscarGasto(Long id) {
+        Optional<Gasto> gasto = repositorio.findById(id);
+        if (gasto.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Gasto no se encontro");
+        } else {
+            return gasto.get();
+        }
     }
 }
